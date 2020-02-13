25248d093ca6c0e893a023c123f9403e113ce92c
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-02-14 07:53:19 +0900

    fix comments for PositionJointModel

--- include/computed_torque_controllers/position_joint_model.hpp
+++ include/computed_torque_controllers/position_joint_model.hpp
@@ -9,6 +9,7 @@
 #include <computed_torque_controllers/acceleration_joint_model.hpp>
 #include <computed_torque_controllers/common_namespaces.hpp>
 #include <control_toolbox/pid.h>
+#include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <joint_limits_interface/joint_limits.h>
 #include <joint_limits_interface/joint_limits_interface.h>
@@ -19,24 +20,28 @@
 #include <ros/node_handle.h>
 #include <urdf/model.h>
 
-#include <dart/dynamics/Joint.hpp>
+#include <Eigen/Core>
 
 #include <boost/foreach.hpp>
 #include <boost/optional.hpp>
 
 namespace computed_torque_controllers {
 
-// ===========================================================================================
-// core control implementation without command subscription
-//   [ input] position and velocity setpoints of each controlled joint & states of all joints
-//   [output] effort commands to controlled joints
+// =======================================================
+// dynamics model which calculates required joint efforts
+// on the basis of desired joint positions
 class PositionJointModel : protected AccelerationJointModel {
 public:
   PositionJointModel() {}
 
   virtual ~PositionJointModel() {}
 
-  // ==========================================
+  // ========================================================
+  // name-based interface for ros-controller implementations
+  // ========================================================
+
+  // ======================================================
+  // initialize the model by params in the given namespace
   bool init(const ros::NodeHandle &param_nh) {
     // init the base model first
     if (!AccelerationJointModel::init(param_nh)) {
@@ -53,7 +58,7 @@ public:
     // compose joint info
     joint_infos_.resize(model_->getNumDofs());
     for (std::size_t i = 0; i < model_->getNumDofs(); ++i) {
-      const std::string &name(model_->getDof(i)->getJoint()->getName());
+      const std::string &name(jointIdToName(i));
       JointInfo &info(joint_infos_[i]);
 
       // load joint limits from robot description if limits exist
@@ -76,9 +81,9 @@ public:
     return true;
   }
 
-  // =============
+  // ===============================================
+  // reset the model states (not destroy the model)
   void reset() {
-    // reset PID & pos_sp_sat_handle
     BOOST_FOREACH (JointInfo &info, joint_infos_) {
       info.pos_sp = std::numeric_limits< double >::quiet_NaN();
       if (info.pos_sp_sat_handle) {
@@ -88,23 +93,27 @@ public:
     }
   }
 
-  //
+  // =====================================
+  // returns joint positions of the model
   std::map< std::string, double > getPositions() const {
     return eigenToJointMap(getPositionsEigen());
   }
 
-  // ====================================================
+  // =====================================
+  // returns joint setpoints of the model
   std::map< std::string, double > getSetpoints() const {
     return eigenToJointMap(getSetpointsEigen());
   }
 
-  // =================================================================
+  // ===============================================================
+  // update model state based on given joint positions & velocities
   void update(const std::map< std::string, double > &positions,
               const std::map< std::string, double > &velocities, const ros::Duration &dt) {
     update(jointMapToEigen(positions), jointMapToEigen(velocities), dt);
   }
 
-  // ========================================================================
+  // ================================================================================
+  // compute joint effort commands to realize given pos setpoints based on the model
   std::map< std::string, double >
   computeEffortCommands(const std::map< std::string, double > &pos_setpoints,
                         const ros::Duration &dt) {
@@ -112,6 +121,10 @@ public:
   }
 
 protected:
+  // ======================================================
+  // index-based interface for child model implementations
+  // ======================================================
+
   Eigen::VectorXd getPositionsEigen() const {
     Eigen::VectorXd q(joint_infos_.size());
     for (std::size_t i = 0; i < joint_infos_.size(); ++i) {
@@ -128,7 +141,8 @@ protected:
     return qd;
   }
 
-  void update(const Eigen::VectorXd &q, const Eigen::VectorXd &dq, const ros::Duration& dt) {
+  void update(const Eigen::VectorXd &q, const Eigen::VectorXd &dq, const ros::Duration &dt) {
+    // update the base model
     AccelerationJointModel::update(q, dq, dt);
 
     for (std::size_t i = 0; i < model_->getNumDofs(); ++i) {
@@ -141,6 +155,9 @@ protected:
 
   std::map< std::string, double > computeEffortCommands(const Eigen::VectorXd &qd,
                                                         const ros::Duration &dt) {
+    // convert position setpoints to acceleration setpoints
+    // by saturating position setpoints according to the joint limits
+    // & applying PID controllers.
     Eigen::VectorXd u(model_->getNumDofs());
     for (std::size_t i = 0; i < model_->getNumDofs(); ++i) {
       JointInfo &info(joint_infos_[i]);
@@ -151,6 +168,7 @@ protected:
       u[i] = info.pid.computeCommand(info.pos_sp - info.pos, dt);
     }
 
+    // calculate effort commands based on the acceleration setpoints
     return AccelerationJointModel::computeEffortCommands(u);
   }
 
